ecd42c26d5ed2c08114356edb4d983ea0ac00530
chanxingxing <cx900605@163.com>
2020-06-07 22:38:43 +0800

    all

--- src/com/lambda/TestLambda.java
+++ src/com/lambda/TestLambda.java
@@ -14,6 +14,10 @@ public class TestLambda {
 
     @Test
     public void test1(){
+        /**
+         * map--接受一个lambda,将元素转成其他形式或者提取信息。
+         *    --接受一个函数作为参数，该函数会被应用到每个元素上，并将其映射成一个新元素。
+         */
         Stream<String> stream = list.stream().map(s -> s.toUpperCase());
         stream.forEach(s->System.out.println(s));
         //list.stream().map(str->str.toUpperCase()).forEach(s -> System.out.println(s));
@@ -27,7 +31,11 @@ public class TestLambda {
         List<Stream<Character>> aList = list.stream().map(TestLambda::filterCharacter).collect(Collectors.toList());
 
 
-       // list.stream().map(TestLambda::filterCharacter).forEach(sm->sm.forEach(ch->System.out.println(ch)));
+       //list.stream().map(TestLambda::filterCharacter).forEach(sm->sm.forEach(ch->System.out.println(ch)));
+        /**
+         * flatMap --接收一个函数作为参数，将流中的每个值都换成另一个流，然后把所有流连接成一个流
+         */
+       list.stream().flatMap(TestLambda::filterCharacter).forEach(System.out::println);
     }
 
     @Test
